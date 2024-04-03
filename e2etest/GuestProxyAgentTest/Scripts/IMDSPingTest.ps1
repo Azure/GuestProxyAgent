6412f7758b1c77fa500102e3e529eddeed0633f7
yinbing <siyin@microsoft.com>
2024-04-03 09:00:23 -0700

    add meta_data header in imds ping test

--- e2etest/GuestProxyAgentTest/Scripts/IMDSPingTest.ps1
+++ e2etest/GuestProxyAgentTest/Scripts/IMDSPingTest.ps1
@@ -4,9 +4,10 @@ while($i -lt 10){
     try {
         $url = "http://169.254.169.254/metadata/instance?api-version=2020-06-01"
         $webRequest = [System.Net.HttpWebRequest]::Create($url)	
+        $webRequest.Headers.Add("Metadata", "True")
         $response = $webRequest.GetResponse()
-        if ($response.StatusCode -eq [System.Net.HttpStatusCode]::OK) {
-            Write-Output "Response status code is OK (200)"
+        if ($response.StatusCode -eq [System.Net.HttpStatusCode]::OK -and $response.Headers.Keys -contains "x-ms-azure-host-authorization") {
+            Write-Output "Response status code is OK (200) and contains authorization header"
         }
         else {
             Write-Error "Ping test failed. Response status code is $($response.StatusCode)"
