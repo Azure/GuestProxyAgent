f33b4247d2bea0452987b9a29d53334ad6f1ad34
Zhidong Peng <zpeng@microsoft.com>
2024-04-15 09:40:01 -0700

    Update build.cmd to make it work for WS2019 (#20)
    * Add ContinueAtConvertBpfToNative at build.cmd to skip this build error.
    * change REM to echo in build.cmd
    * fix xcopy command issue (source folder cannot be end with '\') at WS2019

--- build.cmd
+++ build.cmd
@@ -3,6 +3,7 @@ SET root_path=%~dp0
 SET out_path=%root_path%out
 set Configuration=%1
 set CleanBuild=%2
+set ContinueAtConvertBpfToNative=%3
 if "%Configuration%"=="" (SET Configuration=debug)
 echo Configuration=%Configuration%
 echo out_path=%out_path%
@@ -15,7 +16,7 @@ SET rustup_version=1.69.0
 
 
 if "%CleanBuild%"=="clean" (
-    REM delete old files
+    echo ======= delete old files
     echo RD /S /Q %out_dir%
     RD /S /Q %out_dir%
 
@@ -23,30 +24,30 @@ if "%CleanBuild%"=="clean" (
     RD /S /Q %root_path%packages
 )
 
-REM nuget restore
+echo ======= nuget restore
 call nuget restore
 if  %ERRORLEVEL% NEQ 0 (
     echo call nuget restore with exit-code: %errorlevel%
     exit /b %errorlevel%
 )
 
-REM rustup update to a particular version
+echo ======= rustup update to a particular version
 call rustup update %rustup_version%
 
-REM create out path folder and subfolder
+echo ======= create out path folder and subfolder
 if not exist "%out_path%" (md "%out_path%")
 if not exist "%out_dir%" (md "%out_dir%")
 
-REM Prepare out-package folder structure
-SET out_package_dir="%out_dir%"\package
+echo ======= Prepare out-package folder structure
+SET out_package_dir=%out_dir%\package
 if not exist "%out_package_dir%" (md "%out_package_dir%")
 SET out_package_proxyagent_dir="%out_package_dir%"\ProxyAgent
 if not exist "%out_package_proxyagent_dir%" (md "%out_package_proxyagent_dir%")
 
-REM copy VB Scripts to Package folder
+echo ======= copy VB Scripts to Package folder
 xcopy /Y %root_path%\Setup\Windows\*.* %out_package_dir%\
 
-REM build ebpf program
+echo ======= build ebpf program
 SET ebpf_path=%root_path%\ebpf
 echo call clang -target bpf -Werror -O2 -c %ebpf_path%\redirect.bpf.c -o %out_dir%\redirect.bpf.o
 call clang -target bpf -Werror -O2 -c %ebpf_path%\redirect.bpf.c -o %out_dir%\redirect.bpf.o
@@ -54,22 +55,25 @@ if  %ERRORLEVEL% NEQ 0 (
     echo call clang failed with exit-code: %errorlevel%
     exit /b %errorlevel%
 )
-REM copy redirect.bpf.o
+echo ======= copy redirect.bpf.o
 xcopy /Y %out_dir%\redirect.bpf.o %out_package_proxyagent_dir%\
-REM convert redirect.bpf.o to redirect.bpf.sys
+echo ======= convert redirect.bpf.o to redirect.bpf.sys
 call %eBPF_for_Windows_bin_path%\export_program_info.exe --clear
 call %eBPF_for_Windows_bin_path%\export_program_info.exe
 echo call powershell.exe %eBPF_for_Windows_bin_path%\Convert-BpfToNative.ps1 -OutDir %out_dir% -FileName redirect.bpf.o -IncludeDir %eBPF_for_Windows_bin_path%
 call powershell.exe %eBPF_for_Windows_bin_path%\Convert-BpfToNative.ps1 -OutDir %out_dir% -FileName redirect.bpf.o -IncludeDir %eBPF_for_Windows_inc_path%
 if  %ERRORLEVEL% NEQ 0 (
     echo call Convert-BpfToNative.ps1 failed with exit-code: %errorlevel%
-    exit /b %errorlevel%
+    if "%ContinueAtConvertBpfToNative%"=="" (
+        exit /b %errorlevel%
+    )
+    echo Skip the error and continue to build other projects
 )
-REM copy redirect.bpf.sys
+echo ======= copy redirect.bpf.sys
 xcopy /Y %out_dir%\redirect.bpf.sys %out_package_proxyagent_dir%\
 xcopy /Y %out_dir%\redirect.bpf.pdb %out_package_proxyagent_dir%\
 
-REM build proxy_agent_shared
+echo ======= build proxy_agent_shared
 set cargo_toml=%root_path%proxy_agent_shared\Cargo.toml
 SET release_flag=
 if "%Configuration%"=="release" (SET release_flag=--release)
@@ -81,13 +85,13 @@ if  %ERRORLEVEL% NEQ 0 (
     exit /b %errorlevel%
 )
 
-REM copy files for run/debug proxy_agent_shared Unit test in VS Code
+echo ======= copy files for run/debug proxy_agent_shared Unit test in VS Code
 echo xcopy /Y /C /Q %out_dir% %out_dir%\deps\
 xcopy /Y /C /Q %out_dir% %out_dir%\deps\
 echo xcopy /Y /S /C /Q %out_dir% %root_path%proxy_agent_shared\target\%Configuration%\
 xcopy /Y /S /C /Q %out_dir% %root_path%proxy_agent_shared\target\%Configuration%\
 
-REM run rust proxy_agent_shared tests
+echo ======= run rust proxy_agent_shared tests
 echo call cargo +%rustup_version% test  %release_flag% --manifest-path %cargo_toml% --target-dir %out_path% -- --test-threads=1
 call cargo +%rustup_version% test  %release_flag% --manifest-path %cargo_toml% --target-dir %out_path% -- --test-threads=1
 if  %ERRORLEVEL% NEQ 0 (
@@ -95,11 +99,11 @@ if  %ERRORLEVEL% NEQ 0 (
     exit /b %errorlevel%
 )
 
-REM copy config file for windows platform
-REM Adding a wildcard (*) to the end of the destination will suppress this prompt and default to copying as a file:
+echo ======= copy config file for windows platform
+echo ======= Adding a wildcard (*) to the end of the destination will suppress this prompt and default to copying as a file:
 xcopy /Y %root_path%proxy_agent\config\GuestProxyAgent.windows.json %out_dir%\GuestProxyAgent.json*
 
-REM build proxy_agent
+echo ======= build proxy_agent
 set cargo_toml=%root_path%proxy_agent\Cargo.toml
 SET release_flag=
 if "%Configuration%"=="release" (SET release_flag=--release)
@@ -111,7 +115,7 @@ if  %ERRORLEVEL% NEQ 0 (
     exit /b %errorlevel%
 )
 
-REM copy files for run/debug proxy_agent Unit test
+echo ======= copy files for run/debug proxy_agent Unit test
 echo xcopy /Y /C /Q %eBPF_for_Windows_bin_path%\EbpfApi.* %out_dir%\
 xcopy /Y /C /Q %eBPF_for_Windows_bin_path%\EbpfApi.* %out_dir%\
 echo xcopy /Y /C /Q %out_dir% %out_dir%\deps\
@@ -119,7 +123,7 @@ xcopy /Y /C /Q %out_dir% %out_dir%\deps\
 echo xcopy /Y /S /C /Q %out_dir% %root_path%proxy_agent\target\%Configuration%\
 xcopy /Y /S /C /Q %out_dir% %root_path%proxy_agent\target\%Configuration%\
 
-REM run rust proxy_agent tests
+echo ======= run rust proxy_agent tests
 echo call cargo +%rustup_version% test  %release_flag% --manifest-path %cargo_toml% --target-dir %out_path% -- --test-threads=1
 call cargo +%rustup_version% test  %release_flag% --manifest-path %cargo_toml% --target-dir %out_path% -- --test-threads=1
 if  %ERRORLEVEL% NEQ 0 (
@@ -127,7 +131,7 @@ if  %ERRORLEVEL% NEQ 0 (
     exit /b %errorlevel%
 )
 
-REM build proxy_agent_extension
+echo ======= build proxy_agent_extension
 SET extension_root_path=%root_path%proxy_agent_extension
 SET extension_src_path=%root_path%proxy_agent_extension\src\windows
 set cargo_toml=%extension_root_path%\Cargo.toml
@@ -139,13 +143,13 @@ if  %ERRORLEVEL% NEQ 0 (
     exit /b %errorlevel%
 )
 
-REM copy files for run/debug proxy_agent_extension Unit test
+echo ======= copy files for run/debug proxy_agent_extension Unit test
 echo xcopy /Y /C /Q %out_dir% %out_dir%\deps\
 xcopy /Y /C /Q %out_dir% %out_dir%\deps\
 echo xcopy /Y /S /C /Q %out_dir% %root_path%proxy_agent_extension\target\%Configuration%\
 xcopy /Y /S /C /Q %out_dir% %root_path%proxy_agent_extension\target\%Configuration%\
 
-REM run rust proxy_agent_extension tests
+echo ======= run rust proxy_agent_extension tests
 echo call cargo +%rustup_version% test  %release_flag% --manifest-path %cargo_toml% --target-dir %out_path% -- --test-threads=1
 call cargo +%rustup_version% test  %release_flag% --manifest-path %cargo_toml% --target-dir %out_path% -- --test-threads=1
 if  %ERRORLEVEL% NEQ 0 (
@@ -153,7 +157,7 @@ if  %ERRORLEVEL% NEQ 0 (
     exit /b %errorlevel%
 )
 
-REM build proxy_agent_setup
+echo ======= build proxy_agent_setup
 set cargo_toml=%root_path%proxy_agent_setup\Cargo.toml
 SET release_flag=
 if "%Configuration%"=="release" (SET release_flag=--release)
@@ -165,7 +169,7 @@ if  %ERRORLEVEL% NEQ 0 (
     exit /b %errorlevel%
 )
 
-REM restore e2e test project dependencies
+echo ======= restore e2e test project dependencies
 echo call dotnet.exe restore %root_path%\e2etest\GuestProxyAgentTest.sln
 call dotnet.exe restore %root_path%\e2etest\GuestProxyAgentTest.sln
 if  %ERRORLEVEL% NEQ 0 (
@@ -173,28 +177,30 @@ if  %ERRORLEVEL% NEQ 0 (
     exit /b %errorlevel%
 )
 
-REM build e2e test project
-echo call dotnet.exe build %root_path%\e2etest\GuestProxyAgentTest.sln --no-restore --configuration %Configuration% -o %out_dir%\e2etest
-call dotnet.exe build %root_path%\e2etest\GuestProxyAgentTest.sln --no-restore --configuration %Configuration% -o %out_dir%\e2etest
+echo ======= build e2e test project
+SET out_e2etest_dir=%out_dir%\e2etest
+echo call dotnet.exe build %root_path%\e2etest\GuestProxyAgentTest.sln --no-restore --configuration %Configuration% -o %out_e2etest_dir%
+call dotnet.exe build %root_path%\e2etest\GuestProxyAgentTest.sln --no-restore --configuration %Configuration% -o %out_e2etest_dir%
 if  %ERRORLEVEL% NEQ 0 (
     echo call dotnet.exe build failed with exit-code: %errorlevel%
     exit /b %errorlevel%
 )
+dir /S /B %out_e2etest_dir%\
 
-REM copy setup tool to Package folder
+echo ======= copy setup tool to Package folder
 xcopy /Y %out_dir%\proxy_agent_setup.exe %out_package_dir%\
 xcopy /Y %out_dir%\proxy_agent_setup.pdb %out_package_dir%\
 
-REM copy to ProxyAgent folder
+echo ======= copy to ProxyAgent folder
 xcopy /Y %out_dir%\GuestProxyAgent.exe %out_package_proxyagent_dir%\
 xcopy /Y %out_dir%\GuestProxyAgent.pdb %out_package_proxyagent_dir%\
 xcopy /Y %out_dir%\GuestProxyAgent.json %out_package_proxyagent_dir%\
 xcopy /Y %out_dir%\EbpfApi.dll %out_package_proxyagent_dir%\
 xcopy /Y %out_dir%\EbpfApi.pdb %out_package_proxyagent_dir%\
 
-SET out_package_proxyagent_extension_dir="%out_package_dir%"\ProxyAgent_Extension
+SET out_package_proxyagent_extension_dir=%out_package_dir%\ProxyAgent_Extension
 if not exist "%out_package_proxyagent_extension_dir%" (md "%out_package_proxyagent_extension_dir%")
-REM copy cmd files
+echo ======= copy ProxyAgent Extension files
 xcopy /Y %extension_src_path%\HandlerManifest.json %out_package_proxyagent_extension_dir%\
 for %%F in (%extension_src_path%\*.cmd) do (
     echo Found file: %%F
@@ -202,11 +208,13 @@ for %%F in (%extension_src_path%\*.cmd) do (
 )
 xcopy /Y %out_dir%\ProxyAgentExt.exe %out_package_proxyagent_extension_dir%\
 
-REM copy e2e test project to Package folder
-xcopy /Y /S /C /Q %out_dir%\e2etest\ %out_package_dir%\e2etest\
+echo ======= copy e2e test project to Package folder
+SET out_package_e2etest_dir=%out_package_dir%\e2etest
+echo xcopy /Y /S /C /Q %out_e2etest_dir% %out_package_e2etest_dir%\
+xcopy /Y /S /C /Q %out_e2etest_dir% %out_package_e2etest_dir%\
 
-REM run binskim command
+echo ======= run binskim command
 call %bin_skim_path%\BinSkim.exe analyze %out_package_proxyagent_dir%\GuestProxyAgent.exe --output %out_package_proxyagent_dir%\GuestProxyAgent.exe.binskim.json --rich-return-code=true --force
 
-REM Generate build-configuration.zip file
+echo ======= Generate build-configuration.zip file
 call powershell.exe Compress-Archive -Path "%out_package_dir%" -DestinationPath "%out_dir%"\build-%Configuration%-windows-amd64.zip" -Force
