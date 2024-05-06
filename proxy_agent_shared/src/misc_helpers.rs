d3d1008b2708390c5baa1bf9d840e127a7fed441
shahneerali <131208062+shahneerali@users.noreply.github.com>
2024-05-06 10:41:35 -0700

    GuestProxyAgent: Translate key status AuthorizationRules and validate details  (#36)

--- proxy_agent_shared/src/misc_helpers.rs
+++ proxy_agent_shared/src/misc_helpers.rs
@@ -79,6 +79,20 @@ where
     Ok(obj)
 }
 
+pub fn json_clone<T>(obj: &T) -> std::io::Result<T>
+where
+    T: Serialize + DeserializeOwned,
+{
+    let json = serde_json::to_string(obj)?;
+    match serde_json::from_str(&json) {
+        Ok(obj) => Ok(obj),
+        Err(e) => Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            e.to_string(),
+        )),
+    }
+}
+
 pub fn get_current_exe_dir() -> PathBuf {
     let mut path = std::env::current_exe().unwrap();
     path.pop();
@@ -311,5 +325,31 @@ mod tests {
         let path = PathBuf::new();
         let file_name = super::get_file_name(path);
         assert_eq!("InvalidPath", file_name, "file_name mismatch");
+    }
+
+    #[test]
+    fn json_clone_test() {
+        let test = TestStruct {
+            thread_id: super::get_thread_identity(),
+            date_time_string_with_miliseconds: super::get_date_time_string_with_miliseconds(),
+            date_time_string: super::get_date_time_string(),
+            date_time_rfc1123_string: super::get_date_time_rfc1123_string(),
+            date_time_unix_nano: super::get_date_time_unix_nano(),
+            long_os_version: super::get_long_os_version(),
+            current_exe_dir: super::get_current_exe_dir().to_str().unwrap().to_string(),
+        };
+
+        let cloned = super::json_clone(&test).unwrap();
+
+        assert_eq!(test.thread_id, cloned.thread_id);
+        assert_eq!(
+            test.date_time_string_with_miliseconds,
+            cloned.date_time_string_with_miliseconds
+        );
+        assert_eq!(test.date_time_string, cloned.date_time_string);
+        assert_eq!(test.date_time_rfc1123_string, cloned.date_time_rfc1123_string);
+        assert_eq!(test.date_time_unix_nano, cloned.date_time_unix_nano);
+        assert_eq!(test.long_os_version, cloned.long_os_version);
+        assert_eq!(test.current_exe_dir, cloned.current_exe_dir);
     }
 }
