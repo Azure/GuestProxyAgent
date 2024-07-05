8d0ac1d4999540f85bc6dc1edf9547fd4c376030
shahneerali <neeralishah@microsoft.com>
2024-07-05 16:45:46 -0700

    clean up

--- e2etest/GuestProxyAgentTest/Utilities/Constants.cs
+++ e2etest/GuestProxyAgentTest/Utilities/Constants.cs
@@ -59,8 +59,7 @@ namespace GuestProxyAgentTest.Utilities
 
         public static bool IS_WINDOWS()
         {
-            //return RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
-            return false;
+            return RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
         }
     }
 }
