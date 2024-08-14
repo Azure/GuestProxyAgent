05250a5dc61c6e343803cbdea572bb8c30249199
shahneerali <neeralishah@microsoft.com>
2024-08-14 12:13:44 -0700

    linter

--- proxy_agent_extension/src/handler_main.rs
+++ proxy_agent_extension/src/handler_main.rs
@@ -97,7 +97,7 @@ fn check_os_version_supported() -> bool {
 fn check_linux_os_supported(version: Version) -> bool {
     let linux_type = linux::get_os_type().to_lowercase();
     if linux_type.contains("ubuntu") {
-        version.major >= constants::MIN_SUPPORTED_UBUNTU_OS_BUILD;
+        version.major >= constants::MIN_SUPPORTED_UBUNTU_OS_BUILD
     } else if linux_type.contains("mariner") {
         return version.major >= constants::MIN_SUPPORTED_MARINER_OS_BUILD;
     } else if linux_type.contains("Linux") {
