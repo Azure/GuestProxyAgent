01eec48f8b0a920fe5500945fb36f208dd084f1a
Zhidong Peng <zpeng@microsoft.com>
2024-08-14 09:03:19 -0700

    Add Azure Linux 3 Fips test group (#100)

--- e2etest/GuestProxyAgentTest/LinuxScripts/InstallGuestProxyAgentPackage.sh
+++ e2etest/GuestProxyAgentTest/LinuxScripts/InstallGuestProxyAgentPackage.sh
@@ -26,10 +26,10 @@ if [[ $os == *"Ubuntu"* ]]; then
     done
 else
     for  i in {1..3}; do
-        echo "start installing unzip via yum $i"
-        sudo yum -y install unzip
+        echo "start installing unzip via dnf $i"
+        sudo dnf -y install unzip
         sleep 10
-        install=$(yum list --installed unzip)
+        install=$(dnf list --installed unzip)
         echo "install=$install"
         if [[ $install == *"unzip"* ]]; then
             echo "unzip installed successfully"
@@ -64,7 +64,7 @@ for i in {1..3}; do
     else
         sudo rpm -i $currentDir/ProxyAgent/packages/*.rpm
         sleep 10
-        install=$(yum list --installed guest-proxy-agent)
+        install=$(dnf list --installed guest-proxy-agent)
         echo "install=$install"
         if [[ $install == *"guest-proxy-agent"* ]]; then
             echo "guest-proxy-agent installed successfully"
