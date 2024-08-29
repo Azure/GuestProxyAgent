81b6ff9f67475c90b13fc28c52f42730bdd64380
shahneerali <neeralishah@microsoft.com>
2024-08-29 09:27:43 -0700

    logger crate

--- proxy_agent/src/common/windows.rs
+++ proxy_agent/src/common/windows.rs
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation
 // SPDX-License-Identifier: MIT
 
-use crate::logger;
 use std::io::{Error, ErrorKind};
 use std::mem::MaybeUninit;
 use windows_sys::Win32::Networking::WinSock;
