6145ffd0c556729701f88860c0e279565a655430
shahneerali <neeralishah@microsoft.com>
2024-09-06 12:50:27 -0700

    fmt

--- proxy_agent_extension/src/linux.rs
+++ proxy_agent_extension/src/linux.rs
@@ -5,16 +5,14 @@ use std::thread;
 #[cfg(not(windows))]
 use std::time::Duration;
 
-use crate::logger;
 use crate::service_main;
-use proxy_agent_shared::{misc_helpers, telemetry};
 
 #[cfg(not(windows))]
 pub fn start_service_wait() {
     // start service
     let service_state = service_main::service_state::ServiceState::new();
     service_main::run(service_state);
-    
+
     loop {
         // continue to sleep until the service is stopped
         thread::sleep(Duration::from_secs(1));
