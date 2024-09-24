cb0562e4801451a89f7d3240de609c0e72d88609
Zhidong Peng <zpeng@microsoft.com>
2024-09-24 13:58:55 -0700

    utilize clap to parse command line arguments (#120)
    * utilize clap to  parse command line arguments
    
    * remove the clap default version command parse
    
    * Update Ubuntu24 image offer and sku
    
    * merge from dev

--- proxy_agent/src/main.rs
+++ proxy_agent/src/main.rs
@@ -16,6 +16,7 @@ pub mod telemetry;
 #[cfg(test)]
 pub mod test_mock;
 
+use clap::{Parser, Subcommand};
 use common::constants;
 use common::helpers;
 use proxy_agent_shared::misc_helpers;
@@ -39,12 +40,36 @@ define_windows_service!(ffi_service_main, proxy_agent_windows_service_main);
 static ASYNC_RUNTIME_HANDLE: tokio::sync::OnceCell<tokio::runtime::Handle> =
     tokio::sync::OnceCell::const_new();
 
-/// This function is the entry point of the GPA process.
-/// azure-proxy-agent /console - launch a long run process of GPA in console mode.
+/// azure-proxy-agent console - launch a long run process of GPA in console mode.
 /// azure-proxy-agent --version - print the version of the GPA.
 /// azure-proxy-agent --status [--wait <seconds>] - get the provision status of the GPA service.
 /// azure-proxy-agent - start the GPA as an OS service.
 ///                     The GPA service will be started as an OS service in the background.
+#[derive(Parser)]
+#[command()]
+struct Cli {
+    /// get the provision status of the GPA service
+    #[arg(short, long)]
+    status: bool,
+
+    /// wait for the provision status to finish
+    #[arg(short, long, requires = "status")]
+    wait: Option<u64>,
+
+    /// print the version of the GPA
+    #[arg(short, long)]
+    version: bool,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// launch a long run process of GPA in console mode
+    Console,
+}
+
 #[tokio::main(flavor = "multi_thread")]
 async fn main() {
     // set the tokio runtime handle
@@ -56,48 +81,45 @@ async fn main() {
     // start the Instant to calculate the elapsed time
     let _time = helpers::get_elapsed_time_in_millisec();
 
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() > 1 {
-        if args[1].to_lowercase() == "console" {
-            // console mode - start GPA as long running process
-            let shared_state = SharedState::new();
-            service::start_service(shared_state.clone());
-            println!("Press Enter to end it.");
-            let mut temp = String::new();
-            let _read = std::io::stdin().read_line(&mut temp);
-            service::stop_service(shared_state.clone());
-        } else if args[1].to_lowercase() == "--version" {
-            // --version command to print the version of the GPA
-            println!("{}", misc_helpers::get_current_version());
-        } else if args[1].to_lowercase() == "--status" {
-            // --status command to get the provision status of the GPA service
-            let mut wait_time: u64 = 0;
-            if args.len() >= 4 && args[2].to_lowercase() == "--wait" {
-                // --wait command to wait for the provision status until the given time in seconds
-                // it is an optional, if not provided then it will query the provision state once by waiting for 0 seconds.
-                wait_time = args[3].parse::<u64>().unwrap_or(0);
-            }
-            let (provision_finished, error_message) = provision::get_provision_status_wait(
-                constants::PROXY_AGENT_PORT,
-                Some(Duration::from_secs(wait_time)),
-            )
-            .await;
-            if !provision_finished {
-                // exit code 1 means provision not finished yet.
-                process::exit(1);
-            } else {
-                // provision finished
-                if !error_message.is_empty() {
-                    // if there is any error message then print it and exit with exit code 2.
-                    println!("{}", error_message);
-                    process::exit(2);
-                }
-                // no error message then exit with 0.
-                return;
-            }
+    let cli = Cli::parse();
+
+    if cli.version {
+        println!("{}", misc_helpers::get_current_version());
+        return;
+    }
+
+    if cli.status {
+        // --wait parameter to wait for the provision status until the given time in seconds
+        // it is an optional, if not provided then it will query the provision state once by waiting for 0 seconds.
+        let wait_time = cli.wait.unwrap_or(0);
+        let (provision_finished, error_message) = provision::get_provision_status_wait(
+            constants::PROXY_AGENT_PORT,
+            Some(Duration::from_secs(wait_time)),
+        )
+        .await;
+        if !provision_finished {
+            // exit code 1 means provision not finished yet.
+            process::exit(1);
         } else {
-            println!("Invalid argument: {}", args[1]);
+            // provision finished
+            if !error_message.is_empty() {
+                // if there is any error message then print it and exit with exit code 2.
+                println!("{}", error_message);
+                process::exit(2);
+            }
+            // no error message then exit with 0.
+            return;
         }
+    }
+
+    if let Some(Commands::Console) = cli.command {
+        // console mode - start GPA as long running process
+        let shared_state = SharedState::new();
+        service::start_service(shared_state.clone());
+        println!("Press Enter to end it.");
+        let mut temp = String::new();
+        let _read = std::io::stdin().read_line(&mut temp);
+        service::stop_service(shared_state.clone());
     } else {
         // no argument provided, start the GPA as an OS service
         #[cfg(windows)]
