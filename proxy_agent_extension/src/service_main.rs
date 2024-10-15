6fdc3bdb4f38ed614a7e313ff1c6dad2b0bcd9cf
shahneerali <131208062+shahneerali@users.noreply.github.com>
2024-10-15 16:26:38 -0700

    Add Failed Authentication Summary to Proxy Agent VM Extension Substatus  (#132)
    * Update product version to 1.0.22
    
    * update product version to 1.0.23
    
    * add auth fail summary
    
    * remove cargo changes from main
    
    * update status reading
    
    * linter
    
    * update plugin name
    
    ---------
    
    Co-authored-by: Zhidong Peng <zpeng@microsoft.com>

--- proxy_agent_extension/src/service_main.rs
+++ proxy_agent_extension/src/service_main.rs
@@ -366,6 +366,15 @@ fn report_proxy_agent_aggregate_status(
                             message: error_message.to_string(),
                         },
                     },
+                    SubStatus {
+                        name: constants::PLUGIN_FAILED_AUTH_NAME.to_string(),
+                        status: constants::TRANSITIONING_STATUS.to_string(),
+                        code: constants::STATUS_CODE_NOT_OK,
+                        formattedMessage: FormattedMessage {
+                            lang: constants::LANG_EN_US.to_string(),
+                            message: error_message.to_string(),
+                        },
+                    },
                 ]
             };
         }
@@ -419,6 +428,15 @@ fn extension_substatus(
                         message: version_mismatch_message.to_string(),
                     },
                 },
+                SubStatus {
+                    name: constants::PLUGIN_FAILED_AUTH_NAME.to_string(),
+                    status: constants::TRANSITIONING_STATUS.to_string(),
+                    code: constants::STATUS_CODE_NOT_OK,
+                    formattedMessage: FormattedMessage {
+                        lang: constants::LANG_EN_US.to_string(),
+                        message: version_mismatch_message.to_string(),
+                    },
+                },
             ]
         };
     }
@@ -461,6 +479,30 @@ fn extension_substatus(
             substatus_proxy_agent_connection_message =
                 "proxy connection summary is empty".to_string();
         }
+        let substatus_failed_auth_message: String;
+        if !proxy_agent_aggregate_status_top_level
+            .failedAuthenticateSummary
+            .is_empty()
+        {
+            let proxy_agent_aggregate_failed_auth_status_obj =
+                proxy_agent_aggregate_status_top_level.failedAuthenticateSummary;
+            match serde_json::to_string(&proxy_agent_aggregate_failed_auth_status_obj) {
+                Ok(proxy_agent_aggregate_failed_auth_status) => {
+                    substatus_failed_auth_message = proxy_agent_aggregate_failed_auth_status;
+                }
+                Err(e) => {
+                    let error_message = format!(
+                        "Error in serializing proxy agent aggregate failed auth status: {}",
+                        e
+                    );
+                    logger::write(error_message.to_string());
+                    substatus_failed_auth_message = error_message;
+                }
+            }
+        } else {
+            logger::write("proxy failed auth summary is empty".to_string());
+            substatus_failed_auth_message = "proxy failed auth summary is empty".to_string();
+        }
 
         status.substatus = {
             vec![
@@ -480,6 +522,15 @@ fn extension_substatus(
                     formattedMessage: FormattedMessage {
                         lang: constants::LANG_EN_US.to_string(),
                         message: substatus_proxy_agent_message.to_string(),
+                    },
+                },
+                SubStatus {
+                    name: constants::PLUGIN_FAILED_AUTH_NAME.to_string(),
+                    status: constants::SUCCESS_STATUS.to_string(),
+                    code: constants::STATUS_CODE_OK,
+                    formattedMessage: FormattedMessage {
+                        lang: constants::LANG_EN_US.to_string(),
+                        message: substatus_failed_auth_message.to_string(),
                     },
                 },
             ]
@@ -801,11 +852,22 @@ mod tests {
             userGroups: Some(vec!["test".to_string()]),
         };
 
+        let proxy_failedAuthenticateSummary_obj = ProxyConnectionSummary {
+            userName: "test".to_string(),
+            ip: "test".to_string(),
+            port: 1,
+            processCmdLine: "test".to_string(),
+            responseStatus: "test".to_string(),
+            count: 1,
+            processFullPath: Some("test".to_string()),
+            userGroups: Some(vec!["test".to_string()]),
+        };
+
         let toplevel_status = GuestProxyAgentAggregateStatus {
             timestamp: misc_helpers::get_date_time_string(),
             proxyAgentStatus: proxy_agent_status_obj,
             proxyConnectionSummary: vec![proxy_connection_summary_obj],
-            failedAuthenticateSummary: vec![],
+            failedAuthenticateSummary: vec![proxy_failedAuthenticateSummary_obj],
         };
 
         let mut status = StatusObj {
@@ -883,6 +945,15 @@ mod tests {
                                 message: "test".to_string(),
                             },
                         },
+                        SubStatus {
+                            name: constants::PLUGIN_FAILED_AUTH_NAME.to_string(),
+                            status: constants::SUCCESS_STATUS.to_string(),
+                            code: constants::STATUS_CODE_OK,
+                            formattedMessage: FormattedMessage {
+                                lang: constants::LANG_EN_US.to_string(),
+                                message: "test".to_string(),
+                            },
+                        },
                     ]
                 },
             };
@@ -898,6 +969,10 @@ mod tests {
             );
             assert_eq!(
                 status.substatus[2].name,
+                constants::PLUGIN_FAILED_AUTH_NAME.to_string()
+            );
+            assert_eq!(
+                status.substatus[3].name,
                 constants::EBPF_SUBSTATUS_NAME.to_string()
             );
 
