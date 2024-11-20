d6e632bfda5bab2df6e23120365dcd658c6f10dc
zhiongpeng <zpeng@microsoft.com>
2024-11-20 22:24:46 +0000

    fix formatting

--- proxy_agent/src/test_mock/server_mock.rs
+++ proxy_agent/src/test_mock/server_mock.rs
@@ -22,11 +22,7 @@ static GUID: Lazy<String> = Lazy::new(|| Uuid::new_v4().to_string());
 static mut CURRENT_STATE: Lazy<String> =
     Lazy::new(|| String::from(key_keeper::MUST_SIG_WIRESERVER));
 
-pub async fn start(
-    ip: String,
-    port: u16,
-    cancellation_token: CancellationToken,
-) {
+pub async fn start(ip: String, port: u16, cancellation_token: CancellationToken) {
     logger::write_information("Mock Server starting...".to_string());
     let addr = format!("{}:{}", ip, port);
     let listener = TcpListener::bind(&addr).await.unwrap();
