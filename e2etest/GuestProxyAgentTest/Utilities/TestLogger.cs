d74525c90d8145ab9c9a1803589ce64380cdfbb8
Zhidong Peng <zpeng@microsoft.com>
2026-03-10 11:20:08 -0700

    Fix Windows GPA VMExtension expectedProxyAgentVersion check logic. (#325)
    * Fix: Privilege match must be case insensitive.
    
    * Add comments to explain why not optimize the query parameters to lower case for the request_url.
    
    * fix format
    
    * Fix Windows GPA VMExtension expectedProxyAgentVersion check logic.
    
    * fix typo
    
    * Try get use the available vm size if configured one is not available.
    
    * fix typo
    
    * catch ErrorCode: AllocationFailed and retry with different VMSize if possible
    
    * Printout Exception Type for catch AllocationFailed ErrorCode
    
    * AllocationFailed but no alternative VM sizes found in the error message, last retry with available VM size
    
    * Use Common TestLogger instead of random ConsoleLog
    
    * Fix TestLogger not initi yet issue at constructor time
    
    * Add one more logging for first VM creation failures

---
+++ e2etest/GuestProxyAgentTest/Utilities/TestLogger.cs
@@ -0,0 +1,17 @@
+﻿namespace GuestProxyAgentTest.Utilities
+{
+    public class TestLogger
+    {
+        public TestLogger(string prefix)
+        {
+            this.Prefix = prefix;
+        }
+
+        public string Prefix { get; }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"[{this.Prefix}] - {DateTime.Now:yyyy-MM-ddTHH:mm:ss.fff} - {message}");
+        }
+    }
+}
